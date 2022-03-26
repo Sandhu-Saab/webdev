fd34f1bd46dc123d854e5fab5eb979532de4f534
“onlypythonuse@gmail.com” <onlypythonuse@gmail.com>
2022-03-26 12:57:44 +0530

    Finish REST Framework Viewsets & Routers

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 from .views import article_list, article_detail, ArticleAPIView
-from .views import Articledetail, GenericAPIView
+from .views import Articledetail, GenericAPIView, ArticleViewSet
 from . import views
+from rest_framework.routers import DefaultRouter
 
 
 
 
-
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
 
 urlpatterns = [
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>', include(router.urls)),
     path('list/', article_list),
     path('detail/<int:pk>/', article_detail),
     path('article_api/', ArticleAPIView.as_view()),
